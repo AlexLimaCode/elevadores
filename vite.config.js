fd61b045741ec35c53a0b77b5b51f71d98b81db1
roblesbraun <roblesbraun@gmail.com>
2021-09-12 21:05:15 -0500

    #14-viteconfig-update-2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, './pages/nosotros.html')
+        main: resolve(__dirname, 'main.js'),
+        nested: resolve(__dirname, './pages/contacto.html'),
+        nested: resolve(__dirname, './pages/faq.html'),
+        nested: resolve(__dirname, './pages/nosotros.html'),
+        nested: resolve(__dirname, './pages/productos.html'),
+        nested: resolve(__dirname, './pages/servicios.html')
       }
     }
   }
