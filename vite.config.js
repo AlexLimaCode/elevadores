14bb645864eaf92c21acd639dae9dc7e0a8a3d5f
AlexLimaCode <alejandro.limama@anahuac.mx>
2021-09-22 14:47:37 -0500

    #32-add-js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ module.exports = defineConfig({
                 faq: resolve(__dirname, './pages/faq.html'),
                 nosotros: resolve(__dirname, './pages/nosotros.html'),
                 productos: resolve(__dirname, './pages/productos.html'),
-                servicios: resolve(__dirname, './pages/servicios.html')
+                servicios: resolve(__dirname, './pages/servicios.html'),
+                js1: resolve(__dirname, './js/jquery.swatchbook.js'),
+                js2: resolve(__dirname, './js/modernizr.custom.79639.js'),
             }
         }
     }
