e3e00b0b09b4dffa0e8a290ce61709b6839175b9
roblesbraun <roblesbraun@gmail.com>
2021-09-21 21:52:10 -0500

    #29-update-viteconfig

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,7 @@ module.exports = defineConfig({
                 faq: resolve(__dirname, './pages/faq.html'),
                 nosotros: resolve(__dirname, './pages/nosotros.html'),
                 productos: resolve(__dirname, './pages/productos.html'),
-                servicios: resolve(__dirname, './pages/servicios.html'),
-                js1: resolve(__dirname, './js/jquery.swatchbook.js'),
-                js2: resolve(__dirname, './js/modernizr.custom.79639.js')
+                servicios: resolve(__dirname, './pages/servicios.html')
             }
         }
     }
