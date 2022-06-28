911b3853f580c823639ed6c7bb0271b334622ad5
Alejandro Lima Martinez <alejandro.lima@cbqasolutions.com>
2022-06-28 13:53:09 -0500

    ultimos cambios

--- main.js
+++ main.js
@@ -84,3 +84,26 @@ for (let i = 0; i < elements.length; i++) {
     });
 }
 
+window.addEventListener('scroll', function(){
+
+    function fade(direction){
+        var Scroleable = document.querySelectorAll('.animate_'+direction);
+        for (let i = 0; i < Scroleable.length; i++) {
+            var altura = window.innerHeight/1.3;
+            var distancia = Scroleable[i].getBoundingClientRect().top;
+            Scroleable[i].classList.add('transform'+direction);
+            if (distancia<=altura) {
+                Scroleable[i].classList.add('aparece');
+            }else{
+                Scroleable[i].classList.remove('aparece');
+            }
+            
+        }
+    }
+    fade('down')
+    fade('right')
+    fade('up')
+    fade('left')
+    fade('right')
+    fade('up')
+})
